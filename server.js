891837d9fc4478abe23ea4018d2af7c07c3a2258
yazanabdulhafez <yazanali31@yahoo.com>
2021-08-30 17:30:33 +0300

    fix

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const indexController = require("./controllers/index.controller");
 const apiDataController = require("./controllers/apiData.controller");
 const { createFavController, getFavController, deleteFavController, updateFavController } = require("./controllers/CRUD.controller");
+const favModel = require("./models/favDrink.model");
 
 require("dotenv").config();
 
@@ -16,12 +17,19 @@ app.use(express.json());
 const PORT = process.env.PORT;
 
 /////////////////////////
-
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true },
-  () => console.log('connected to the DB'));
-
+const mongoUrl = 'mongodb+srv://Yazan:yazan12345@cluster1.x17fh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("Database connected!"))
+  .catch(err => console.log(err));
 /////////////////////
-
+const seedUserData = () => {
+  const tea = new favModel({
+    strDrink: "tea",
+    strDrinkThumb: "sugar lot resd nom"
+  });
+  console.log(tea);
+  tea.save();
+};
+seedUserData();
 app.get('/', indexController)
 app.get('/drinks', apiDataController);
 
